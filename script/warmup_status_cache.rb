ccc154bbb663c475d36f4d4c75e511af04bebbef
Rafael Garcia <r@ragar.me>
2014-07-27 19:32:39 -0500

    pass param to cache warmup script

--- script/warmup_status_cache.rb
+++ script/warmup_status_cache.rb
@@ -1,6 +1,7 @@
+abort "rails runner script/warmup_status_cache.rb :status_count" if ARGV.count < 1
 Station.all.each do |station|
   p "Station #{station.id}"
-  statuses = StationStatus.where(station_id: station.id).order(id: :desc).limit(50000)
+  statuses = StationStatus.where(station_id: station.id).order(id: :desc).limit(ARGV[0].to_i)
   compressed_statuses = Snappy.deflate(statuses.to_json)
   $redis.set( "status_#{station.id}", compressed_statuses)
 end
